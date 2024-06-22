195f02d61604e396300391ac3da798408aff9ab5
Steve Murimi <murimi101@gmail.com>
2024-06-22 18:37:03 +0000

    Minr changes to Sign-In process

--- frontend-react-js/src/pages/ConfirmationPage.js
+++ frontend-react-js/src/pages/ConfirmationPage.js
@@ -1,9 +1,9 @@
 import './ConfirmationPage.css';
 import React from "react";
 import { useParams } from 'react-router-dom';
-import {ReactComponent as Logo} from '../components/svg/logo.svg';
+import { ReactComponent as Logo } from '../components/svg/logo.svg';
 
-// Authenication
+// Authentication
 import { Auth } from 'aws-amplify';
 
 export default function ConfirmationPage() {
@@ -17,60 +17,67 @@ export default function ConfirmationPage() {
   const code_onchange = (event) => {
     setCode(event.target.value);
   }
+  
   const email_onchange = (event) => {
     setEmail(event.target.value);
   }
 
   const resend_code = async (event) => {
- 
-    setErrors('')
+    setErrors('');
     try {
       await Auth.resendSignUp(email);
       console.log('code resent successfully');
-      setCodeSent(true)
+      setCodeSent(true);
     } catch (err) {
-      // does not return a code
-      // does cognito always return english
-      // for this to be an okay match?
-      console.log(err)
-      if (err.message == 'Username cannot be empty'){
-        setErrors("You need to provide an email in order to send Resend Activiation Code")   
-      } else if (err.message == "Username/client id combination not found."){
-        setErrors("Email is invalid or cannot be found.")   
+      console.log(err);
+      if (err.message === 'Username cannot be empty') {
+        setErrors("You need to provide an email in order to send Resend Activation Code");
+      } else if (err.message === "Username/client id combination not found.") {
+        setErrors("Email is invalid or cannot be found.");
       }
     }
   }
 
   const onsubmit = async (event) => {
     event.preventDefault();
-    setErrors('')
+    setErrors('');
     try {
       await Auth.confirmSignUp(email, code);
-      window.location.href = "/"
+  // Redirect user to sign-in page instead of home page.
+      window.location.href = "/signin"
     } catch (error) {
-      setErrors(error.message)
+      setErrors(error.message);
     }
-    return false
+    return false;
   }
 
   let el_errors;
-  if (errors){
+  if (errors) {
     el_errors = <div className='errors'>{errors}</div>;
   }
 
-
   let code_button;
-  if (codeSent){
-    code_button = <div className="sent-message">A new activation code has been sent to your email</div>
+  if (codeSent) {
+    code_button = <div className="sent-message">A new activation code has been sent to your email</div>;
   } else {
     code_button = <button className="resend" onClick={resend_code}>Resend Activation Code</button>;
   }
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     if (params.email) {
-      setEmail(params.email)
+      setEmail(params.email);
     }
-  }, [])
+  }, [params.email]);
+
+  // Get email from the signup page where we stored the email in localStorage
+  React.useEffect(() => {
+    const storedEmail = localStorage.getItem('email');
+    // check if the email is set, if it's not set then we will ignore it, and use the value typed in the email box:
+    if (storedEmail) {
+      // Filling the Email
+      setEmail(storedEmail);
+    }
+  }, []);
 
   return (
     <article className="confirm-article">
@@ -89,7 +96,7 @@ export default function ConfirmationPage() {
               <input
                 type="text"
                 value={email}
-                onChange={email_onchange} 
+                onChange={email_onchange}
               />
             </div>
             <div className='field text_field code'>
@@ -97,7 +104,7 @@ export default function ConfirmationPage() {
               <input
                 type="text"
                 value={code}
-                onChange={code_onchange} 
+                onChange={code_onchange}
               />
             </div>
           </div>
